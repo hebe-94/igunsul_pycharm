77889316f145ddd611b5d1ce414371b96e21d0c5
shinsehoon <iszzon@gmail.com>
2021-02-17 00:10:26 +0900

    [진행] 테이블 접근 완료, tr의 개수로 반복문 [진행 예정] tr의 개수 반복문 사용하여 데이터 뽑아오기

--- igunsul/makeData.py
+++ igunsul/makeData.py
@@ -2,13 +2,13 @@
 
 from selenium import webdriver
 from time import sleep
+
 # 크롬 웹 드라이버 경로 설정
 
 driver = webdriver.Chrome('C:/chromedriver/chromedriver.exe')
 
 # 크롬을 통해 로그인화면 접속
 driver.get('https://igunsul.net')
-
 
 # 아이디와 비밀번호 입력 (0.3초 간격으로)
 sleep(0.5)
@@ -29,18 +29,29 @@ driver.execute_script("arguments[0].click();", sample)
 # driver.find_element_by_css_selector('#headerMyRoomTabWrap > div:nth-child(3)').click()
 
 
-#웹페이지의 소스코드를 파싱하기 위해 Beautiful Soup 라이브러리 호출
+# 웹페이지의 소스코드를 파싱하기 위해 Beautiful Soup 라이브러리 호출
 from bs4 import BeautifulSoup
 
 driver.get("http://www.igunsul.net/myroom")
 driver.find_element_by_css_selector('#headerMyRoomTabWrap > div:nth-child(3)').click()
+
+# 반복문으로 반복작업 하기
+# 아이디어 1 > table의 tr의 수를 세서 반복문의 횟수를 지정
+table = driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/table/tbody')
+tr = table.find_elements_by_tag_name("tr")
+
+# 반복문 사용해서 각 페이지 들어가고, 데이터 뽑아 오기(여기부터 진행 할것)
+# 리스트가 된다면 DB로 올기는 것으로 (예정)
+
 html = driver.page_source
-soup = BeautifulSoup(html,'lxml')
+soup = BeautifulSoup(html, 'lxml')
 print(soup)
 
+print('tr의 개수')
+print(len(tr))
 print('여기까지')
 
 # 사업명 하나씩 꺼내오기
-title_list = soup.find_all('a','list2detailAnchor listColormyroom_list myroom_list')
-for title in title_list:
-    print(title.text.strip())
+# title_list = soup.find_all('a','list2detailAnchor listColormyroom_list myroom_list')
+# for title in title_list:
+#     print(title.text.strip())
